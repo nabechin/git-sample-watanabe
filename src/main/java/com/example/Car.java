63d8dc6e6a9159ddf499451bb0337a7eda3550e8
nabenabe0258 <nabenabe0258@gmail.com>
2019-08-01 15:10:36 +0900

    Carクラスを追加しました

---
+++ src/main/java/com/example/Car.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class Car {
+	
+  private int speed;
+  private String bodyColor;
+public int getSpeed() {
+	return speed;
+}
+public void setSpeed(int speed) {
+	this.speed = speed;
+}
+public String getBodyColor() {
+	return bodyColor;
+}
+public void setBodyColor(String bodyColor) {
+	this.bodyColor = bodyColor;
+}
+
+}
